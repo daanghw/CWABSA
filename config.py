3ad7ae0762a2faf40d5b49d7aa37dade55e22b98
daanghw <52723633+daanghw@users.noreply.github.com>
2019-07-11 19:08:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ FLAGS = tf.app.flags.FLAGS
 #general variables
 tf.app.flags.DEFINE_string('embedding_type','BERT_Large','can be: glove, word2vec-cbow, word2vec-SG, fasttext, BERT, BERT_Large, ELMo')
 tf.app.flags.DEFINE_integer("year",2016, "year data set [2014]")
-tf.app.flags.DEFINE_integer('embedding_dim', 1024, 'dimension of word embedding')
+tf.app.flags.DEFINE_integer('embedding_dim', 768, 'dimension of word embedding')
 tf.app.flags.DEFINE_integer('batch_size', 20, 'number of example per batch')
 tf.app.flags.DEFINE_integer('n_hidden', 300, 'number of hidden unit')
 tf.app.flags.DEFINE_float('learning_rate', 0.07, 'learning rate')
